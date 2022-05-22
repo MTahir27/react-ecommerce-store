9ab41b63bd9f1c812cc8836be39f25a38f897b18
MTahir27 <m.tahiridrees27@gmail.com>
2022-05-22 10:52:24 +0500

    Navbar Styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import WebFont from "webfontloader";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Error_404 } from "./Pages/Error_404";
+import { Layout } from "./Pages/Layout";
 
 function App() {
   useEffect(() => {
@@ -16,7 +17,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
         <Route path="/*" element={<Error_404 />} />
       </Routes>
     </BrowserRouter>
---
+++ src/Pages/Layout/index.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Container, Navbar, Nav, Button, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+
+export const Layout = () => {
+  const [authStatus, setAuthStatus] = useState(true);
+
+  return (
+    <>
+      <Navbar bg="light" className="navbar">
+        <Container>
+          <Navbar.Brand>
+            <Link to={"/"}>Ecommerce Store</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            {authStatus === true ? (
+              <Nav className="ms-auto my-2 my-lg-0 gap-3 align-items-center">
+                <Link to={"/products"} className="paragraph">
+                  Products
+                </Link>
+                <Link to={"/checkout"} className="paragraph">
+                  <FiShoppingCart /> <Badge bg="secondary">0</Badge>
+                  <span className="visually-hidden">unread messages</span>
+                </Link>
+                <Button
+                  variant="link"
+                  type="button"
+                  className="text-decoration-none text-color"
+                >
+                  Logout
+                </Button>
+              </Nav>
+            ) : (
+              <Nav className="ms-auto my-2 my-lg-0 gap-3">
+                <Link to={"/SignUp"}>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="text-uppercase"
+                  >
+                    SignUp
+                  </Button>
+                </Link>
+                <Link to={"/login"}>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="text-uppercase"
+                  >
+                    login
+                  </Button>
+                </Link>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+};
